8e42f04c012c36ae1ede90290287ca2d90173cd2
arikoda <arikoda72126@gmail.com>
2020-08-28 21:49:27 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def configure_permitted_parameters
+      added_attrs = [ :family_name, :first_name, :nickname, :family_n_ruby, :first_n_ruby, :birthday ]        
+      devise_parameter_sanitizer.permit(:sign_up, keys: added_attrs)
+      devise_parameter_sanitizer.permit(:sign_in, keys: )
+      devise_parameter_sanitizer.permit(:acount_update, keys: added_attrs)
+    end
 end
+
