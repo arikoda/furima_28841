d2a0b82e5683fd9cc0e234bcf67de1c040f43ded
arikoda <arikoda72126@gmail.com>
2020-08-28 20:46:32 +0900

    usersテーブルにカラム追加

--- db/migrate/20200828082049_devise_create_users.rb
+++ db/migrate/20200828082049_devise_create_users.rb
@@ -6,15 +6,21 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nicknaame,          null: false
+      t.string :famly_name,         null: false
+      t.string :first_name,         null: false
+      t.string :family_n_ruby,      null: false
+      t.string :first_n_ruby,       null: false
+      t.date :birthday,             null:false
 
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
-      ## Rememberable
+      ##Recoverable
       t.datetime :remember_created_at
 
-      ## Trackable
+      ## Recoverable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
       # t.datetime :last_sign_in_at
