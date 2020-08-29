334cf7ae7041a4c14d2ec84d7725d7dc21406215
arikoda <arikoda72126@gmail.com>
2020-08-29 21:52:51 +0900

    gemのインストール（rspec,FactoryBot,Faker)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,6 @@ class User < ApplicationRecord
   validates :first_name,format: { with:/\A[ぁ-んァ-ンー-龥]/ }
   validates :family_n_ruby,format: { with:/\A[ァ-ヶー-]+\z/ }
   validates :first_n_ruby,format: { with:/\A[ァ-ヶー-]+\z/ }
-  validates :mail,format: { with:/\A\S+@\S+\.\S+\z/}
+  validates :email,format: { with:/\A\S+@\S+\.\S+\z/}
 
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    email                    {Faker::Internet.free_email} 
+    password                 = Faker::Internet.password(min_length: 6) 
+    password                 {password}
+    password_confirmation    {password} 
+    nickname                 {Faker::Name.initials(number: 3)}
+    birthday                 {Faker::Date.birthday(min_age:18, max_age:90)}
+    family_name              {"鈴木"}
+    first_name               {"太郎"}
+    family_n_ruby            {"スズキ"}
+    first_n_ruby             {"タロー"}
+  end
+end 
