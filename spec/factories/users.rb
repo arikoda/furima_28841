03333f2a2652c1eb326b26757b87ff676456d3ab
arikoda <arikoda72126@gmail.com>
2020-08-31 17:54:54 +0900

    テストコード作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,10 @@
 FactoryBot.define do
   factory :user do
     email                    {Faker::Internet.free_email} 
-    password                 = Faker::Internet.password(min_length: 6) 
-    password                 {password}
+    password                 {"geoir8e"}
     password_confirmation    {password} 
-    nickname                 {Faker::Name.initials(number: 3)}
-    birthday                 {Faker::Date.birthday(min_age:18, max_age:90)}
+    nickname                 { Faker::Name.initials(number: 3) }
+    birthday                 { Faker::Date.birthday(min_age:18, max_age:90) }
     family_name              {"鈴木"}
     first_name               {"太郎"}
     family_n_ruby            {"スズキ"}
