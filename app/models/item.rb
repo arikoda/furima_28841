bbd2065c7c0a4fa3336bc802e4c9b5366941c080
arikoda <arikoda72126@gmail.com>
2020-09-02 12:25:42 +0900

    バリデーションの設定

---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category    
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :origin_area
+  belongs_to_active_hash :send_day
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :image, presence: true
+  validates :price, presence: true,  
+      numericality: { only_integer: true, greater_than: 299, less_than: 9999999 },
+      format: { with: /\A\d+\z/ }
+  validates :category_id, :condition_id, :origin_area_id, :shipping_charge_id, :send_day_id,
+      presence: true, numericality: { other_than:1 }
+  validates :user, presence: true
+end
