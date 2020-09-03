5dfaeb71a286dcc9a35b9494f7af1207fe766530
arikoda <arikoda72126@gmail.com>
2020-09-03 14:43:23 +0900

    itemコントローラーのcreateアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end    
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :explanation, :price, :category_id, :condition_id, :origin_area_id, :shipping_charges_id, :send_day_id).merge(user_id: current_user.id)
   end
 end
