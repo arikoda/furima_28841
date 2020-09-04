68a424815ac69951be72a7151f079f95553c0eff
arikoda <arikoda72126@gmail.com>
2020-09-04 14:54:44 +0900

    モデル単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category    
+  belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_charge
   belongs_to_active_hash :origin_area
@@ -8,23 +8,13 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
 
-  validates :image, presence: true, if: :was_attached?
-  
-  #def image_content_type
-    #extension = ['image/png', 'image/jpg', 'image/jpeg']
-    #errors.add(:image, "の拡張子が間違っています") unless image.content_type.in?(extension)
-  #end
-
-  def was_attached?
-    self.image.attached?
-  end
-  
+  validates :image, presence: true
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :price, presence: true,  
-      numericality: { only_integer: true, greater_than: 299, less_than: 9999999 },
-      format: { with: /\A\d+\z/ }
+  validates :price, presence: true,
+                    numericality: { only_integer: true, greater_than: 299, less_than: 9_999_999 },
+                    format: { with: /\A\d+\z/ }
   validates :category_id, :condition_id, :origin_area_id, :shipping_charges_id, :send_day_id,
-      presence: true, numericality: { other_than:1 }
+            presence: true, numericality: { other_than: 1 }
   validates :user, presence: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,84 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user_id: user.id)
+    @item.image = fixture_file_upload('camera.png')
+  end
+
+  describe '商品出品機能' do
+    context '商品出品ができるとき' do
+      it 'image,name,category,condition,shipping_charge,origin_area,send_day全て入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品ができないとき' do
+      it '写真が添付されていないと保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '状態が選択されていないと保存できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank", 'Condition is not a number')
+      end
+
+      it '商品の説明がないと保存できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+
+      it 'カテゴリー情報が選択されていないと保存できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it '配送料負担の選択がされていないと保存できない' do
+        @item.shipping_charges_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charges can't be blank")
+      end
+
+      it '発送元の地域についての情報が選択されていないと保存できない' do
+        @item.origin_area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Origin area can't be blank", 'Origin area is not a number')
+      end
+
+      it '発送日数が選択されていないと保存できない' do
+        @item.send_day_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Send day can't be blank")
+      end
+
+      it '価格情報が入力されていないと保存できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '価格情報が300円以下で設定されていると保存できない' do
+        @item.price = 200
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than 299')
+      end
+
+      it '販売価格が全角で入力されていると保存できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'ユーザーが紐づいていない商品は出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
