1d40309fe5671376ce7b962120086b967b676fab
arikoda <arikoda72126@gmail.com>
2020-09-07 15:03:53 +0900

    rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only:[:show, :edit]
+  before_action :set_item, only: [:show, :edit]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
 
   def update
     item = Item.find(params[:id])
-    item.update(item_params) 
+    item.update(item_params)
     redirect_to action: :show
   end
 
