8d451a9c11389162ce670ce76a8962d6286c7d87
arikoda <arikoda72126@gmail.com>
2020-09-10 22:40:30 +0900

    コードの整理

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,14 +7,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :send_day
   has_one_attached :image
   belongs_to :user
+  has_one :order
 
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :explanation, presence: true
-  validates :price, presence: true,
-                    numericality: { only_integer: true, greater_than: 299, less_than: 9_999_999 },
-                    format: { with: /\A\d+\z/ }
-  validates :category_id, :condition_id, :origin_area_id, :shipping_charge_id, :send_day_id,
-            presence: true, numericality: { other_than: 1 }
-  validates :user, presence: true
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explanation
+    validates :price,numericality: { only_integer: true, greater_than: 299, less_than: 9_999_999 },format: { with: /\A\d+\z/ }
+    validates :category_id, :condition_id, :origin_area_id, :shipping_charge_id, :send_day_id,numericality: { other_than: 1 }
+    validates :user
+  end
 end
